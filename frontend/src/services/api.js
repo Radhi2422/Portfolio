23a6611a570924973e9eea908be95082c697bf83
Radhika Mittal <106824237+Radhi2422@users.noreply.github.com>
2026-07-18 12:26:53 +0530

    Delete frontend/src/services directory

--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
- const REACT_APP_BASE_URL=process.env.REACT_APP_BASE_URL
-      
-const api = axios.create({
-  baseURL: `${REACT_APP_BASE_URL}`
-});
-
-export default api;
