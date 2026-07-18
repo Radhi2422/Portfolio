f3321bf1d73c94e84a9a652a6b69482d48225bf3
Radhika Mittal <106824237+Radhi2422@users.noreply.github.com>
2026-07-18 12:20:35 +0530

    Add files via upload
    some files removed as there is issue in deployment in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard.jsx"
-import { CodeDashboard } from "./pages/Dashboard/CodeDashboard.jsx";
-import ProblemPage from "./pages/Code Editor/ProblemPage.jsx";
 import DataStructurepage from "./pages/DSA/DataStructurepage.jsx";
 import MyProjectsTypesPage from "./pages/Projects/MyProjectsTypesPage.jsx";
 import MyExperience from "./pages/Experience/MyExperience.jsx";
@@ -11,9 +9,6 @@ function App() {
     <BrowserRouter>
     <Routes>
         <Route path="/" element={<Dashboard />} />
-        {/* Coding platform addition */}
-        <Route path="/codedashboard" element={<CodeDashboard/>}/>
-        <Route path="/problems" element={<ProblemPage />} />
         {/* DSA */}
         <Route path="/dsa" element={<DataStructurepage/>}/>
         {/* MyProjects */}
